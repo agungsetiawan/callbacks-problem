a9463b6086d5bf3f2e29eb0fd3ad715f794c1bdb
Agung Setiawan <com.agungsetiawan@gmail.com>
2020-03-19 09:43:48 +0700

    Create test for AssetType#accuracy_status

--- test/models/asset_type_test.rb
+++ test/models/asset_type_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AssetTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # This test will fail
+  #
+  # test "can get accuracy status, accuracy > 100" do
+  #   asset_type = create(:asset_type, accuracy: 150)
+
+  #   assert_equal "warning", asset_type.accuracy_status
   # end
+
+  test "can get accuracy status, accuracy > 100" do
+    asset_type = create(:asset_type, quantity_on_hand: 1)
+    create(:asset_inventory, asset_type: asset_type)
+    create(:asset_inventory, asset_type: asset_type)
+
+    assert_equal "warning", asset_type.accuracy_status
+  end
 end
